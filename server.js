c5e49641360a5023faedbde3390b2881f42185e4
Bharatsharma <108224876+bharat24862486@users.noreply.github.com>
2023-05-27 01:50:42 +0530

    url

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 // server.js
+// const jsonServer = require('json-server')
+// const server = jsonServer.create()
+// const router = jsonServer.router('db.json')
+// const middlewares = jsonServer.defaults()
+ 
+// server.use(middlewares)
+// server.use('', router)
+// server.listen(process.env.PORT || 5000, () => {
+//   console.log('JSON Server is running')
+// })
+
+
+// server.js
+
+const path = require('path');
+
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+
+
+const fs = require('fs')
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
+const router = jsonServer.router(db)
+
+// const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
  
+
+
 server.use(middlewares)
 server.use('', router)
 server.listen(process.env.PORT || 5000, () => {
   console.log('JSON Server is running')
 })
+
